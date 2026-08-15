7e2f417895dd14e07d51c8342afda4ca74479ab4
mamax-dev <maxwell.aslima@gmail.com>
2026-08-15 07:31:40 -0300

    Delete app/retry_policy.py

--- app/retry_policy.py
+++
@@ -1,6 +0,0 @@
-def is_retryable_error(exc: Exception) -> bool:
-    message = str(exc).upper()
-    return any(token in message for token in [
-        "429", "RESOURCE_EXHAUSTED", "RATE LIMIT",
-        "503", "UNAVAILABLE", "504", "DEADLINE_EXCEEDED",
-    ])
