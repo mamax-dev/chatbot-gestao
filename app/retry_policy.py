c5706f9705c01fa8565e9ecc9aa9f019cf1eb4da
mamax-dev <maxwell.aslima@gmail.com>
2026-08-15 08:58:13 -0300

    Delete app directory

--- app/retry_policy.py
+++
@@ -1,3 +0,0 @@
-def is_retryable_error(exc:Exception)->bool:
-    m=str(exc).upper()
-    return any(x in m for x in ['408','429','RESOURCE_EXHAUSTED','RATE LIMIT','503','UNAVAILABLE','504','DEADLINE_EXCEEDED','TIMEOUT'])
